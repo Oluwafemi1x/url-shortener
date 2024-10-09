79f1a78b0d0567f9c1e6564fdaee2c6bbd421e3f
Oluwafemi1x <freshfemi1990@gmail.com>
2024-10-09 18:45:28 +0800

    Newly updated line

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router'; // Import RouterModule
 import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ShortenUrlComponent } from './shorten-url.component';
-import { AppRoutingModule } from './app-routing.module';
 import { UrlShortenerService } from './shorten-url.service';
 
 
